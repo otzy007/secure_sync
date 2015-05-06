a155b130ef1a4c94e9254c906bd76e3da61f1468
Andrei Gliga <otzy_007@yahoo.com>
2015-05-06 10:27:38 +0300

    backup files

---
+++ Backup/models/my_backup.rb
@@ -0,0 +1,72 @@
+# encoding: utf-8
+
+##
+# Backup Generated: my_backup
+# Once configured, you can run the backup with the following command:
+#
+# $ backup perform -t my_backup [-c <path_to_configuration_file>]
+#
+# For more information about Backup's components, see the documentation at:
+# http://backup.github.io/backup
+#
+Model.new(:my_backup, 'Description for my_backup') do
+  ##
+  # Split [Splitter]
+  #
+  # Split the backup file in to chunks of 250 megabytes
+  # if the backup file size exceeds 250 megabytes
+  #
+  split_into_chunks_of 250
+  ##
+  # Archive [Archive]
+  #
+  # Adding a file or directory (including sub-directories):
+  #   archive.add "/path/to/a/file.rb"
+  #   archive.add "/path/to/a/directory/"
+  #
+  # Excluding a file or directory (including sub-directories):
+  #   archive.exclude "/path/to/an/excluded_file.rb"
+  #   archive.exclude "/path/to/an/excluded_directory
+  #
+  # By default, relative paths will be relative to the directory
+  # where `backup perform` is executed, and they will be expanded
+  # to the root of the filesystem when added to the archive.
+  #
+  # If a `root` path is set, relative paths will be relative to the
+  # given `root` path and will not be expanded when added to the archive.
+  #
+  #   archive.root '/path/to/archive/root'
+  #
+  archive :my_archive do |archive|
+    # Run the `tar` command using `sudo`
+    # archive.use_sudo
+    archive.add "/path/to/a/file.rb"
+    archive.add "/path/to/a/folder/"
+    archive.exclude "/path/to/a/excluded_file.rb"
+    archive.exclude "/path/to/a/excluded_folder"
+  end
+
+  ##
+  # Local (Copy) [Storage]
+  #
+  store_with Local do |local|
+    local.path       = "~/backups/"
+    local.keep       = 5
+  end
+
+  ##
+  # OpenSSL [Encryptor]
+  #
+  encrypt_with OpenSSL do |encryption|
+    encryption.password      = "my_password"            # From String
+    encryption.password_file = "/path/to/password/file" # Or from File
+    encryption.base64        = true
+    encryption.salt          = true
+  end
+
+  ##
+  # Gzip [Compressor]
+  #
+  compress_with Gzip
+
+end
